bdcf26923384401d021dd426bad6937a36288f36
Tristan van Bokkem <tristanvanbokkem@gmail.com>
2014-04-18 16:01:04 +0200

    Added definitions for Ignore plugin

---
+++ definitions.Ignore.php
@@ -0,0 +1,7 @@
+<?php
+// Copyright 2014 Tristan van Bokkem
+
+// Dutch Definitions for the Ignore plugin.
+
+$definitions["Ignore member"] = "Negeer lid";
+$definitions["Unignore member"] = "Negeren ongedaan maken";
