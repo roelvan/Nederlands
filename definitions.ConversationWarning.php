28c8a2dbc862c5dedc4dea7a4926e22a189a3fcf
Tristan van Bokkem <tristanvanbokkem@gmail.com>
2014-04-21 18:13:20 +0200

    Added ConversationWarning plugin definitions

--- definitions.ConversationWarning.php
+++ definitions.ConversationWarning.php
@@ -1,6 +1,9 @@
 <?php
 // Copyright 2014 Tristan van Bokkem
 
-// English Definitions for the ConversationWarning plugin.
+// Dutch Definitions for the ConversationWarning plugin.
 
-$definitions["Notice:"] = "Let Op:";
+$definitions["Add a Converstation Warning"] = "Voeg een waarschuwing toe";
+$definitions["Define the rules of a Conversation"] = "Bepaal de regels van een onderwerp";
+$definitions["Edit Warning"] = "Bewerk Waarschuwing";
+$definitions["Remove Warning"] = "Verwijder Waarschuwing";
