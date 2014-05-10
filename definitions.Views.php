4327431822654859bcf3ae3bfaa42826d9ff2f93
Tristan van Bokkem <tristanvanbokkem@gmail.com>
2014-05-10 11:51:28 +0200

    Updated comment

--- definitions.Views.php
+++ definitions.Views.php
@@ -1,7 +1,7 @@
 <?php
 // Copyright 2014 Tristan van Bokkem
 
-// Dutch Definitions for the Profiles plugin.
+// Dutch Definitions for the Views plugin.
 
 $definitions["gambit.order by views"] = "sorteer op aantal keer bekeken";
 $definitions["%s view"] = "%sx bekeken";
