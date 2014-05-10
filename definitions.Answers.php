09ae32e3a8336962d6677686ba8a19493c3d6ab8
Tristan van Bokkem <tristanvanbokkem@gmail.com>
2014-05-10 11:46:02 +0200

    Updated Copyright and comment

--- definitions.Answers.php
+++ definitions.Answers.php
@@ -1,8 +1,7 @@
 <?php
-// Copyright 2013 Toby Zerner, Simon Zerner
-// This file is part of esoTalk. Please see the included license file for usage information.
+// Copyright 2014 Tristan van Bokkem
 
-// English Definitions for the Answers plugin.
+// Dutch Definitions for the Answers plugin.
 
 $definitions["Answer"] = "Antwoord";
 $definitions["Answered"] = "Beantwoord";
