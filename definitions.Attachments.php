9323064e2a5afce2a516c720295bf9582c8a867d
Tristan van Bokkem <tristanvanbokkem@gmail.com>
2014-05-10 11:46:43 +0200

    Updated Copyright and comment

--- definitions.Attachments.php
+++ definitions.Attachments.php
@@ -1,8 +1,7 @@
 <?php
-// Copyright 2013 Toby Zerner, Simon Zerner
-// This file is part of esoTalk. Please see the included license file for usage information.
+// Copyright 2014 Tristan van Bokkem
 
-// English Definitions for the Answers plugin.
+// Dutch Definitions for the Attachament plugin.
 
 $definitions["Attach a file"] = "Voeg een bijlage toe";
 $definitions["Drop files to upload"] = "Sleep bestanden hierin om te uploaden";
