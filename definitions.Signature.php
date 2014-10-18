dbed3e578f63ec48afa33e3daf2dc02faa80d103
tvb <tristanvanbokkem@gmail.com>
2014-10-18 18:48:52 +0200

    Added missing translation definition

--- definitions.Signature.php
+++ definitions.Signature.php
@@ -3,5 +3,6 @@
 
 // Dutch Definitions for the Signature plugin.
 
+$definitions["BBCode Allowed"] = "BBCode toegestaan";
 $definitions["Signature"] = "Handtekening";
 $definitions["Max characters:"] = "Max karakters:";
