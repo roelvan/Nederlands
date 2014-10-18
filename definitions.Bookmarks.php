f2ff88353ef93942ccfa3a35168bd7fff0d3d321
tvb <tristanvanbokkem@gmail.com>
2014-10-18 18:39:09 +0200

    Added translations for the Bookmarks plugin

---
+++ definitions.Bookmarks.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright 2014 Tristan van Bokkem
+
+// Dutch Definitions for the Bookmarks plugin.
+
+$definitions["Bookmark"] = "Sla op als favoriet";
+$definitions["Unbookmark"] = "Verwijder als favoriet";
+$definitions["gambit.bookmarked"] = "Favoriet";
+$definitions["label.bookmarked"] = "Favoriet";
+
